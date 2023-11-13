6de6ef7fc9734edc75736c87f7e4a9d7621200d1
MalaRuze <vojtech.ruzic@gmail.com>
2023-11-13 17:52:30 +0100

    feat: add mapping of user hosted events in dashboard

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,13 +1,12 @@
-import { currentUser } from "@clerk/nextjs";
-import { Card } from "@/components/ui/card";
 import AddIcon from "@mui/icons-material/Add";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import prisma from "../../../../prisma/client";
+import { getUserEvents } from "@/lib/events";
+import { Event } from "@prisma/client";
+import { getTimeFromDate } from "@/lib/utils";
 
 const DashboardPage = async () => {
-  const user = await currentUser();
-
+  const { events } = await getUserEvents();
   return (
     <main className="flex min-h-screen flex-col px-24 pt-16 gap-8">
       <h1 className="text-2xl font-bold">Events</h1>
@@ -15,12 +14,24 @@ const DashboardPage = async () => {
         <Link href="/new-event">
           <Button
             variant="outline"
-            className="h-60 flex flex-col items-center justify-around p-20"
+            className="h-60 flex flex-col items-center justify-around p-20 w-full"
           >
             <AddIcon className="text-4xl text-primary" />
             <p>Create event</p>
           </Button>
         </Link>
+        {events?.map((event: Event) => (
+          <Link href={`/event/${event.id}`} key={event.id}>
+            <Button
+              variant="outline"
+              className="h-60 flex flex-col items-center justify-around p-20 w-full"
+            >
+              <h2>{event.title}</h2>
+              <p>{event.dateFrom.getDate()}</p>
+              <p>{getTimeFromDate(event.dateFrom)}</p>
+            </Button>
+          </Link>
+        ))}
       </div>
     </main>
   );
