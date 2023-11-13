72d10bcebb89ce7cc72285de8a47c02700be4f0f
MalaRuze <vojtech.ruzic@gmail.com>
2023-11-13 17:44:42 +0100

    feat: create about page route

---
+++ src/app/(routes)/about/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      about us, we are great
+    </main>
+  );
+};
+
+export default DashboardPage;
