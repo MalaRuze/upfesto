7d050a2fa4f756cb311e2943db91969db8a5ed75
MalaRuze <vojtech.ruzic@gmail.com>
2023-11-15 09:35:52 +0100

    feat: add server action for creating new attendance

--- src/app/_actions.tsx
+++ src/app/_actions.tsx
@@ -2,11 +2,12 @@
 
 import prisma from "../../prisma/client";
 import { z } from "zod";
-import { NewEventFormDataSchema } from "@/lib/schema";
+import { AttendanceFormDataSchema, NewEventFormDataSchema } from "@/lib/schema";
 import { revalidatePath } from "next/cache";
-import { Response } from "@prisma/client";
+import { ResponseEnum } from "@prisma/client";
 
 type NewEventInputs = z.infer<typeof NewEventFormDataSchema>;
+type NewAttendanceInputs = z.infer<typeof AttendanceFormDataSchema>;
 
 export async function createNewEvent(data: NewEventInputs) {
   const validation = NewEventFormDataSchema.safeParse(data);
@@ -37,10 +38,77 @@ export async function createNewEvent(data: NewEventInputs) {
     data: {
       eventId: event.id,
       userId: data.hostId,
-      response: Response.YES,
+      response: ResponseEnum.YES,
     },
   });
 
   revalidatePath("/dashboard");
   return { success: true, event };
+}
+
+export async function createNewAttendance(data: NewAttendanceInputs) {
+  const validation = AttendanceFormDataSchema.safeParse(data);
+  if (!validation.success) {
+    return { success: false, error: validation.error.errors };
+  }
+
+  //check if attendance already exists
+  const existingAttendance = await prisma.attendance.findUnique({
+    where: {
+      userId_eventId: {
+        eventId: data.eventId,
+        userId: data.userId,
+      },
+    },
+  });
+
+  //if attendance exists, update the response
+  if (existingAttendance) {
+    //if response is undefined, delete the attendance
+    if (data.response === undefined) {
+      await prisma.attendance.delete({
+        where: {
+          userId_eventId: {
+            eventId: data.eventId,
+            userId: data.userId,
+          },
+        },
+      });
+      revalidatePath(`/event/${data.eventId}`);
+      return { success: true, attendance: null };
+    }
+
+    //if response is the same, return the existing attendance
+    if (existingAttendance.response === data.response) {
+      return { success: true, attendance: existingAttendance };
+    }
+
+    //if response is different, update the attendance
+    const attendance = await prisma.attendance.update({
+      where: {
+        userId_eventId: {
+          eventId: data.eventId,
+          userId: data.userId,
+        },
+      },
+      data: {
+        response: data.response,
+      },
+    });
+    revalidatePath(`/event/${data.eventId}`);
+    return { success: true, attendance };
+  }
+
+  //if attendance does not exist, create it
+  if (data.response !== undefined) {
+    const attendance = await prisma.attendance.create({
+      data: {
+        eventId: data.eventId,
+        userId: data.userId,
+        response: data.response,
+      },
+    });
+    revalidatePath(`/event/${data.eventId}`);
+    return { success: true, attendance };
+  }
 }
