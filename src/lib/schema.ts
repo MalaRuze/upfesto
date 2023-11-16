a9de88199a55628ed4be34a447ce13fb4bbb414a
MalaRuze <vojtech.ruzic@gmail.com>
2023-11-16 11:43:05 +0100

    feat: add new schema for updating event

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -22,6 +22,25 @@ export const NewEventFormDataSchema = z.object({
   hostId: z.string(),
 });
 
+export const UpdateEventFormDataSchema = z.object({
+  id: z.string(),
+  title: z.string().min(1),
+  description: z.string().optional(),
+  location: z.string().optional(),
+  imageUrl: z.string().optional(),
+  dateFrom: z.date(),
+  timeFrom: z.string().refine((value) => timeFormat.test(value), {
+    message: "Time must be in format HH:MM",
+  }),
+  dateTo: z.date().optional(),
+  timeTo: z
+      .string()
+      .refine((value) => timeFormat.test(value), {
+        message: "Time must be in format HH:MM",
+      }).optional(),
+  hostId: z.string(),
+});
+
 export const AttendanceFormDataSchema = z.object({
   eventId: z.string(),
   userId: z.string(),
