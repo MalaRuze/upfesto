d6415e6e2d78090977ee21d99383bc8f15318718
MalaRuze <vojtech.ruzic@gmail.com>
2023-11-22 17:23:04 +0100

    refactor: improve error handling and data checking in event dialog

--- src/components/EventHandlerDialog.tsx
+++ src/components/EventHandlerDialog.tsx
@@ -11,10 +11,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Event } from "@prisma/client";
 import { cn, getTimeFromDate } from "@/lib/utils";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
-import { createNewEvent, updateEvent } from "@/app/_actions";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { AlertCircle, CalendarIcon } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { createEventAction, updateEventAction } from "@/app/_actions";
+import { CalendarIcon } from "lucide-react";
 import {
   Form,
   FormControl,
@@ -30,7 +29,7 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
-import { format } from "date-fns";
+import { format, isSameDay } from "date-fns";
 import { Calendar } from "@/components/ui/calendar";
 import {
   Select,
@@ -51,6 +50,7 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { useToast } from "@/components/ui/use-toast";
+import PlacesSearchBox from "@/components/PlacesSearchBox";
 
 type CreateProps = {
   mode: "create";
@@ -63,6 +63,7 @@ type UpdateProps = {
   event: Event;
 };
 
+// function to get the correct schema based on the mode
 const getEventFormSchema = (mode: "create" | "update") => {
   switch (mode) {
     case "create":
@@ -74,41 +75,17 @@ const getEventFormSchema = (mode: "create" | "update") => {
 
 const EventHandlerDialog = (props: CreateProps | UpdateProps) => {
   const { user } = useUser();
-  const { toast } = useToast();
   const router = useRouter();
-  const [includeEndDateTime, setIncludeEndDateTime] = useState(false);
+  const { toast } = useToast();
+  // state to keep track of whether the end date and time should be included
+  const [includeEndDateTime, setIncludeEndDateTime] = useState(
+    props.mode === "update" && props.event.dateTo !== null,
+  );
+  // state to keep track of whether the dialog is open
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  // get the correct schema based on the mode
   const schema = getEventFormSchema(props.mode);
-  const form = useForm<z.infer<typeof schema>>({
-    resolver: zodResolver(schema),
-    defaultValues: async () => {
-      switch (props.mode) {
-        case "update":
-          return {
-            id: props.event.id,
-            title: props.event.title,
-            description: props.event.description
-              ? props.event.description
-              : undefined,
-            location: props.event.location ? props.event.location : undefined,
-            imageUrl: props.event.imageUrl ? props.event.imageUrl : undefined,
-            dateCreated: props.event.dateCreated,
-            dateFrom: props.event.dateFrom,
-            timeFrom: getTimeFromDate(props.event.dateFrom),
-            dateTo: props.event.dateTo ? props.event.dateTo : undefined,
-            timeTo: props.event.dateTo
-              ? getTimeFromDate(props.event.dateTo)
-              : undefined,
-            hostId: props.event.hostId,
-          } as z.infer<typeof schema>;
-        case "create":
-          return {
-            timeFrom: "12:00",
-            hostId: user?.id,
-          } as z.infer<typeof schema>;
-      }
-    },
-  });
+  // times to be used in the time select in format HH:MM
   const times: string[] = [];
   for (let i = 0; i < 24; i++) {
     for (let j = 0; j < 60; j += 15) {
@@ -118,11 +95,58 @@ const EventHandlerDialog = (props: CreateProps | UpdateProps) => {
     }
   }
 
+  // function to get default values for the form
+  const getDefaultValues = () => {
+    switch (props.mode) {
+      case "update":
+        return {
+          id: props.event.id,
+          title: props.event.title,
+          description: props.event.description,
+          locationAddress: props.event.locationAddress,
+          locationLon: Number(props.event.locationLon),
+          locationLat: Number(props.event.locationLat),
+          imageUrl: props.event.imageUrl,
+          dateCreated: props.event.dateCreated,
+          dateFrom: props.event.dateFrom,
+          timeFrom: props.event.dateFrom
+            ? getTimeFromDate(props.event.dateFrom)
+            : null,
+          dateTo: props.event.dateTo,
+          timeTo: props.event.dateTo
+            ? getTimeFromDate(props.event.dateTo)
+            : null,
+          hostId: props.event.hostId,
+        } as z.infer<typeof schema>;
+      case "create":
+        return {
+          title: "",
+          timeFrom: "12:00",
+          hostId: user?.id,
+          dateFrom: new Date(),
+          description: "",
+        } as z.infer<typeof schema>;
+    }
+  };
+
+  // initialize form
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: getDefaultValues(),
+  });
+
+  // reset form when props change
+  useEffect(() => {
+    form.reset(getDefaultValues());
+  }, [props, user]);
+
+  // function to handle form submission
   const onSubmit = async (values: z.infer<typeof schema>) => {
+    // create new event or update existing event
     const res =
       props.mode === "create"
-        ? await createNewEvent(values)
-        : await updateEvent({ ...values, id: props.event.id });
+        ? await createEventAction(values)
+        : await updateEventAction({ ...values, id: props.event.id });
 
     if (!res) {
       toast({
@@ -132,18 +156,25 @@ const EventHandlerDialog = (props: CreateProps | UpdateProps) => {
       });
       return;
     }
-    if (res.error) {
+    if (res.success === false) {
       toast({
         title: "Something went wrong",
-        description: "Please try again later. Error: " + res.error[0].message,
+        description: res.error + " Please try again later.",
         variant: "destructive",
       });
       return;
     }
 
+    toast({
+      title: "Event saved",
+      description: "Your event has been saved.",
+    });
+
+    // reset form and close dialog
     form.reset();
-    router.push(`/event/${res.event?.id}`);
     setIsDialogOpen(false);
+    // redirect to new event page
+    router.push(`/event/${res.event?.id}`);
   };
 
   return (
@@ -151,11 +182,16 @@ const EventHandlerDialog = (props: CreateProps | UpdateProps) => {
       onOpenChange={() => {
         form.reset();
         setIsDialogOpen(!isDialogOpen);
+        if (props.mode === "create") {
+          setIncludeEndDateTime(false);
+        }
       }}
       open={isDialogOpen}
     >
+      {/*Dialog Trigger*/}
       <DialogTrigger asChild>{props.trigger}</DialogTrigger>
       <DialogContent>
+        {/*Dialog Header*/}
         <DialogHeader>
           <DialogTitle>
             {props.mode === "create" ? "Create Event" : "Update Event"}
@@ -166,6 +202,8 @@ const EventHandlerDialog = (props: CreateProps | UpdateProps) => {
               : "Update an existing event"}
           </DialogDescription>
         </DialogHeader>
+
+        {/*Form*/}
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
@@ -186,7 +224,6 @@ const EventHandlerDialog = (props: CreateProps | UpdateProps) => {
                 </FormItem>
               )}
             />
-
             {/*Start Date and Time*/}
             <div className="flex gap-4">
               {/*DateFrom*/}
@@ -221,12 +258,18 @@ const EventHandlerDialog = (props: CreateProps | UpdateProps) => {
                           selected={field.value}
                           onSelect={(date) => {
                             field.onChange(date);
+                            // check if dateTo is before dateFrom
                             if (
-                              form.watch("dateTo") !== undefined &&
-                              date !== undefined &&
+                              includeEndDateTime &&
+                              form.watch("dateTo") &&
+                              date &&
                               form.watch("dateTo")! < date
                             ) {
-                              form.setValue("dateTo", date);
+                              const dateToPlusOne = new Date(date);
+                              dateToPlusOne.setDate(
+                                dateToPlusOne.getDate() + 1,
+                              );
+                              form.setValue("dateTo", dateToPlusOne);
                             }
                           }}
                           disabled={(date) =>
@@ -251,7 +294,21 @@ const EventHandlerDialog = (props: CreateProps | UpdateProps) => {
                   <FormItem className="w-full">
                     <FormLabel>Start Time</FormLabel>
                     <Select
-                      onValueChange={field.onChange}
+                      onValueChange={(time) => {
+                        field.onChange(time);
+                        // check if timeTo is before timeFrom
+                        if (
+                          form.watch("dateFrom") &&
+                          form.watch("dateTo") &&
+                          isSameDay(
+                            form.watch("dateFrom"),
+                            form.watch("dateTo")!,
+                          ) &&
+                          time > form.watch("timeTo")!
+                        ) {
+                          form.setValue("timeTo", time);
+                        }
+                      }}
                       defaultValue={field.value}
                     >
                       <FormControl>
@@ -261,7 +318,7 @@ const EventHandlerDialog = (props: CreateProps | UpdateProps) => {
                       </FormControl>
 
                       <SelectContent>
-                        <ScrollArea className="h-72 w-full ">
+                        <ScrollArea className="h-72 w-full">
                           {times.map((time, index) => (
                             <SelectItem key={index} value={time}>
                               {time}
@@ -277,7 +334,6 @@ const EventHandlerDialog = (props: CreateProps | UpdateProps) => {
                 )}
               />
             </div>
-
             {/*End Date and Time*/}
             {includeEndDateTime && (
               <div className="flex gap-4">
@@ -297,6 +353,7 @@ const EventHandlerDialog = (props: CreateProps | UpdateProps) => {
                                 "w-[240px] pl-3 text-left font-normal",
                                 !field.value && "text-muted-foreground",
                               )}
+                              disabled={form.watch("dateFrom") === undefined}
                             >
                               {field.value ? (
                                 format(field.value, "PPP")
@@ -310,10 +367,13 @@ const EventHandlerDialog = (props: CreateProps | UpdateProps) => {
                         <PopoverContent className="w-auto p-0" align="start">
                           <Calendar
                             mode="single"
-                            selected={field.value}
-                            onSelect={field.onChange}
+                            selected={field.value ? field.value : undefined}
+                            onSelect={(date) => {
+                              field.onChange(date);
+                            }}
                             disabled={(date) =>
-                              date < form.watch("dateFrom") ||
+                              (date < form.watch("dateFrom") &&
+                                !isSameDay(date, form.watch("dateFrom"))) ||
                               date < new Date("1900-01-01")
                             }
                             initialFocus
@@ -336,20 +396,23 @@ const EventHandlerDialog = (props: CreateProps | UpdateProps) => {
                       <FormLabel>End Time</FormLabel>
                       <Select
                         onValueChange={field.onChange}
-                        defaultValue={field.value}
+                        defaultValue={field.value ? field.value : undefined}
                       >
                         <FormControl>
-                          <SelectTrigger>
+                          <SelectTrigger
+                            disabled={form.watch("dateTo") === undefined}
+                          >
                             <SelectValue placeholder="Pick a time" />
                           </SelectTrigger>
                         </FormControl>
 
                         <SelectContent>
-                          <ScrollArea className="h-72 w-full ">
+                          <ScrollArea className="h-72  w-full ">
                             {times.map((time, index) => {
                               // condition to make sure that the end time is after the start time
                               if (
                                 form.watch("dateTo") !== undefined &&
+                                form.watch("dateFrom") !== null &&
                                 form.watch("dateFrom").toDateString() ===
                                   form.watch("dateTo")?.toDateString() &&
                                 time < form.watch("timeFrom")
@@ -374,19 +437,46 @@ const EventHandlerDialog = (props: CreateProps | UpdateProps) => {
                 />
               </div>
             )}
+            {/*Include End Date and Time*/}
             <Button
               variant="link"
               type="button"
               className="p-0"
               onClick={() => {
-                setIncludeEndDateTime(!includeEndDateTime);
-                includeEndDateTime
-                  ? form.setValue("dateTo", undefined)
-                  : form.setValue("timeTo", undefined);
+                const newIncludeEndDateTime = !includeEndDateTime;
+                setIncludeEndDateTime(newIncludeEndDateTime);
+                // set end date and time to start date + 1 day at 12:00 if end date and time is included, otherwise set to null
+                if (newIncludeEndDateTime) {
+                  const dateFromPlusOne = new Date(form.watch("dateFrom"));
+                  dateFromPlusOne.setDate(dateFromPlusOne.getDate() + 1);
+                  form.setValue("dateTo", dateFromPlusOne);
+                  form.setValue("timeTo", "12:00");
+                } else {
+                  form.setValue("dateTo", null);
+                  form.setValue("timeTo", null);
+                }
               }}
             >
               {includeEndDateTime ? "- " : "+ "}End Date and Time
             </Button>
+            {/*Location*/}
+            <PlacesSearchBox
+              onSelectAddress={(locationAddress, locationLat, locationLon) => {
+                form.setValue("locationAddress", locationAddress);
+                form.setValue("locationLat", locationLat);
+                form.setValue("locationLon", locationLon);
+              }}
+              defaultValue={
+                props.mode === "update" && props.event.locationAddress !== null
+                  ? props.event.locationAddress
+                  : undefined
+              }
+            />
+            <FormMessage>
+              {form.formState.errors.locationAddress?.message}
+              {form.formState.errors.locationLon?.message}
+              {form.formState.errors.locationLat?.message}
+            </FormMessage>
             {/*Description*/}
             <FormField
               name="description"
@@ -407,6 +497,7 @@ const EventHandlerDialog = (props: CreateProps | UpdateProps) => {
                 </FormItem>
               )}
             />
+            {/*Dialog Footer with Submit Button*/}
             <DialogFooter>
               <Button type="submit">
                 {props.mode === "create" ? "Create event" : "Save changes"}
