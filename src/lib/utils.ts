ee86061417a169209871107a8dd064931e48c4b7
MalaRuze <vojtech.ruzic@gmail.com>
2023-11-22 17:19:35 +0100

    feat: create new utils function to help with error message handling

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,27 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
+export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
-}
+};
 
-export function getTimeFromDate(date: Date): string {
+export const getTimeFromDate = (date: Date): string => {
   const hours = date.getHours().toString().padStart(2, "0");
   const minutes = date.getMinutes().toString().padStart(2, "0");
   return `${hours}:${minutes}`;
-}
+};
+
+export const getErrorMessage = (error: unknown) => {
+  let message: string;
+
+  if (error instanceof Error) {
+    message = error.message;
+  } else if (error && typeof error === "object" && "message" in error) {
+    message = String(error.message);
+  } else if (typeof error === "string") {
+    message = error;
+  } else {
+    message = "Something went wrong";
+  }
+  return message;
+};
