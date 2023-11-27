64634b2c93984573c9824244a811374cdebf4ee0
MalaRuze <vojtech.ruzic@gmail.com>
2023-11-27 15:44:19 +0100

    feat: create new utils functions for getting time in right format to display on frontend

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { isSameDay } from "date-fns";
 
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
@@ -45,10 +46,76 @@ export const getFormattedDateTime = (dateFrom: Date, dateTo?: Date) => {
     if (toYear !== currentYear) {
       formattedDateTo += `, ${toYear}`;
     }
+    if (isSameDay(dateFrom, dateTo)) {
+      return formattedDateFrom;
+    }
     return `${formattedDateFrom} - ${formattedDateTo}`;
   }
 
   return formattedDateFrom;
+};
+
+export const getDetailedFormattedDateTime = (dateFrom: Date, dateTo?: Date) => {
+  const dateFromOptions: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    month: "short",
+    day: "numeric",
+  };
+
+  const dateToOptions: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    month: "short",
+    day: "numeric",
+  };
+
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour: "numeric",
+    minute: "numeric",
+  };
+
+  const currentYear = new Date().getFullYear();
+  const fromYear = dateFrom.getFullYear();
+  const toYear = dateTo?.getFullYear();
+
+  let formattedDateFrom = dateFrom.toLocaleString("en-GB", dateFromOptions);
+  if (fromYear !== currentYear) {
+    formattedDateFrom += `, ${fromYear}`;
+  }
+
+  let formattedTimeFrom = dateFrom.toLocaleString("en-GB", timeOptions);
+
+  if (dateTo) {
+    let formattedDateTo = dateTo.toLocaleString("en-GB", dateToOptions);
+    if (toYear !== currentYear) {
+      formattedDateTo += `, ${toYear}`;
+    }
+
+    let formattedTimeTo = dateTo.toLocaleString("en-GB", timeOptions);
+
+    if (isSameDay(dateFrom, dateTo)) {
+      return {
+        date: `${formattedDateFrom}`,
+        time: `${formattedTimeFrom} - ${formattedTimeTo}`,
+      };
+    } else {
+      return {
+        date: `${formattedDateFrom} - ${formattedDateTo}`,
+        time: formattedTimeFrom,
+      };
+    }
+  }
+
+  return {
+    date: formattedDateFrom,
+    time: formattedTimeFrom,
+  };
+};
+
+export const splitStringAtFirstComma = (
+  str: string,
+): { firstPart: string; rest: string } => {
+  const [firstPart, ...rest] = str.split(",");
+  return { firstPart: firstPart.trim(), rest: rest.join(",").trim() };
 };
 
 export const getErrorMessage = (error: unknown) => {
