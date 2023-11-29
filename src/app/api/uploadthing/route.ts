19516f72dcd78eefd8a6dc32f44800aee91002f3
MalaRuze <vojtech.ruzic@gmail.com>
2023-11-29 10:21:07 +0100

    fix: change uploadthing back to node function

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -2,7 +2,7 @@ import { createNextRouteHandler } from "uploadthing/next";
 
 import { imageFileRouter } from "./core";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 // Export routes for Next App Router
 export const { GET, POST } = createNextRouteHandler({
   router: imageFileRouter,
