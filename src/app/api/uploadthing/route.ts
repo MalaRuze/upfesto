8d01748eb393413e54d451a0b5174052e0cc1b37
MalaRuze <vojtech.ruzic@gmail.com>
2023-11-29 09:14:09 +0100

    fix: make uploadthing edge function

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -2,6 +2,7 @@ import { createNextRouteHandler } from "uploadthing/next";
 
 import { imageFileRouter } from "./core";
 
+export const runtime = "edge";
 // Export routes for Next App Router
 export const { GET, POST } = createNextRouteHandler({
   router: imageFileRouter,
