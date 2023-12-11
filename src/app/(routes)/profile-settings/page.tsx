3693ce90d8b0549636966ce342bc8a22155f923d
MalaRuze <vojtech.ruzic@gmail.com>
2023-12-11 11:07:50 +0100

    feat: add profile settings page

---
+++ src/app/(routes)/profile-settings/page.tsx
@@ -0,0 +1,11 @@
+import { UserProfile } from "@clerk/nextjs";
+
+const ProfileSettingsPage = () => {
+  return (
+    <main>
+      <UserProfile />
+    </main>
+  );
+};
+
+export default ProfileSettingsPage;
